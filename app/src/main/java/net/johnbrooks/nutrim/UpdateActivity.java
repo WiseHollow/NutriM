fc8282c148613ac59f80ace226f6db7ae4748b8c
WiseHollow <ieatlotz@gmail.com>
2017-05-25 16:23:23 -0400

    Added Nutritionix_API_Wrapper.jar as a library. Added functionality for searching for foods.

--- app/src/main/java/net/johnbrooks/nutrim/UpdateActivity.java
+++ app/src/main/java/net/johnbrooks/nutrim/UpdateActivity.java
@@ -2,12 +2,50 @@ package net.johnbrooks.nutrim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class UpdateActivity extends AppCompatActivity {
+import net.johnbrooks.NW.NutritionIXItem;
+import net.johnbrooks.NW.NutritionIXQuery;
+
+public class UpdateActivity extends AppCompatActivity
+{
+    private EditText et_search;
+    private LinearLayout ll_searchContents;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        et_search = (EditText) findViewById(R.id.updateActivity_editText_search);
+        ll_searchContents = (LinearLayout) findViewById(R.id.updateActivity_linearLayout_searchContents);
+        findViewById(R.id.updateActivity_button_search).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ll_searchContents.removeAllViews();
+
+                String keyword = et_search.getText().toString();
+                NutritionIXItem[] items = NutritionIXQuery.searchForItems(keyword);
+
+                for (NutritionIXItem item : items)
+                {
+                    LinearLayout layout = (LinearLayout) getLayoutInflater().inflate(R.layout.widget_updateresult, null);
+
+                    TextView itemName = (TextView) layout.findViewById(R.id.widget_updateResult_itemName);
+                    TextView calories = (TextView) layout.findViewById(R.id.widget_updateResult_caloriesTextView);
+
+                    itemName.setText(item.getName());
+                    calories.setText("Calories: " + item.getCalories());
+
+                    ll_searchContents.addView(layout);
+                }
+            }
+        });
     }
 }
