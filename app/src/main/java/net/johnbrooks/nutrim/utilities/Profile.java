b7aee3c3e20ed568ed8ca635f6ee7f5cf6cc4869
WiseHollow <ieatlotz@gmail.com>
2017-06-06 14:17:58 -0400

    Food items will expire after the day changes.

--- app/src/main/java/net/johnbrooks/nutrim/utilities/Profile.java
+++ app/src/main/java/net/johnbrooks/nutrim/utilities/Profile.java
@@ -76,16 +76,22 @@ public class Profile
 
         profile = new Profile(fullName, birthday, weightKg, heightCm);
 
-        Set<String> consumed = preferences.getStringSet("consumed", new HashSet<String>());
-        for (String s: consumed)
+        String latestDayUsed = preferences.getString("latestDayUsed", null);
+
+        if (latestDayUsed == null || dateFormat.format(new Date()).equalsIgnoreCase(latestDayUsed))
         {
-            NutritionIXItem ixItem = NutritionIXItem.serialize(s);
-            if (ixItem != null)
-                profile.getItemsConsumed().add(ixItem);
+            Set<String> consumed = preferences.getStringSet("consumed", new HashSet<String>());
+            for (String s: consumed)
+            {
+                NutritionIXItem ixItem = NutritionIXItem.serialize(s);
+                if (ixItem != null)
+                    profile.getItemsConsumed().add(ixItem);
+            }
         }
 
-
         profile.setCaloriesToday(calories);
+        if (latestDayUsed != null)
+            profile.setLatestDayUsed(latestDayUsed);
         if (measurementSystem != null)
             profile.setMeasurementSystem(measurementSystem);
         return profile;
@@ -95,6 +101,8 @@ public class Profile
     private Date birthday;
     private int weightKg;
     private int heightCm;
+
+    private String latestDayUsed;
 
     private List<NutritionIXItem> itemsConsumed;
     private int caloriesToday;
@@ -112,6 +120,7 @@ public class Profile
         this.caloriesDailyMax = calculateDailyCalorieNeeds();
         this.measurementSystem = MeasurementSystem.METRIC;
         this.itemsConsumed = new ArrayList<>();
+        this.latestDayUsed = dateFormat.format(new Date());
     }
 
     public String getFullName()
@@ -227,6 +236,7 @@ public class Profile
         editor.putString("birthday", birthdayString);
         editor.putString("measurement", measurementSystem.name());
         editor.putInt("calories", getCaloriesToday());
+        editor.putString("latestDayUsed", getLatestDayUsed());
 
         Set<String> itemsConsumedStrings = new HashSet<>();
         for (NutritionIXItem ixItem : itemsConsumed)
@@ -249,6 +259,16 @@ public class Profile
     public void setMeasurementSystem(MeasurementSystem measurementSystem)
     {
         this.measurementSystem = measurementSystem;
+    }
+
+    public String getLatestDayUsed()
+    {
+        return latestDayUsed;
+    }
+
+    public void setLatestDayUsed(String latestDayUsed)
+    {
+        this.latestDayUsed = latestDayUsed;
     }
 
     public enum MeasurementSystem
