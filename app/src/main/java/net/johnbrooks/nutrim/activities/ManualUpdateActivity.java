23410d30b56f8fe034a17c5775dd9a38f62b47fb
WiseHollow <ieatlotz@gmail.com>
2017-06-13 13:46:42 -0400

    Added ManualUpdateActivity. Added button for manual update.

---
+++ app/src/main/java/net/johnbrooks/nutrim/activities/ManualUpdateActivity.java
@@ -0,0 +1,17 @@
+package net.johnbrooks.nutrim.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.johnbrooks.nutrim.R;
+
+public class ManualUpdateActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_update);
+    }
+}
